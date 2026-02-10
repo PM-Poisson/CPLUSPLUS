70989580fee698f96fc1cc3519dfcd2ea4172fc4
Paul-Malo <paul-malo.poisson@cpe.fr>
2026-02-10 17:30:21 +0100

    ex2 created

---
+++ tp2_template/exercice_2/src/bezier.hpp
@@ -0,0 +1,16 @@
+#ifndef BEZIER_HPP
+#define BEZIER_HPP
+#include <ostream>
+class bezier {
+    float P[4];
+    
+public :
+    bezier();
+    bezier(float p0, float p1, float p2, float p3);
+    float coeff(int i) const;
+    float& coeff(int i);
+    float operator()(float s) const;
+    friend std::ostream& operator<<(std::ostream& os, const bezier& b);
+};
+
+#endif
