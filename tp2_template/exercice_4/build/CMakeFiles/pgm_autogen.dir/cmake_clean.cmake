713070063a187b61289858c7aa1bfcd3282540b3
Paul-Malo <paul-malo.poisson@cpe.fr>
2026-02-12 09:50:37 +0100

    ex4 finished

---
+++ tp2_template/exercice_4/build/CMakeFiles/pgm_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pgm_autogen"
+  "pgm_autogen/mocs_compilation.cpp"
+  "pgm_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pgm_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
