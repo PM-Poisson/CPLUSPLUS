72e896ea5f3515c49ab7eb9e74bebaefdb696ea7
Paul-Malo <paul-malo.poisson@cpe.fr>
2026-02-24 14:13:36 +0100

    yeaa

--- TP_QT/partie_01/src/hello_world.cpp
+++ TP_QT/partie_01/src/hello_world.cpp
@@ -10,12 +10,15 @@ int main(int argc, char *argv[])
 
   //create a button
   QPushButton helloButton("Hello World");
+  QPushButton quitButton("Quit");
 
   //set the size of the button
   helloButton.resize(100, 30);
+  quitButton.resize(100, 30);
 
   //set the visibility of the button
   helloButton.show();
+  quitButton.show();
 
   //start the application
   return app.exec();
